1cb74e9d172c9309d69fe5a66e098c1264f95cb7
Kyle Fossman <kjfossman@gmail.com>
2021-01-22 20:38:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function breakOut(array, changeValue, stopValue){
-  for(i = 0, i<array.length, i++){
+  for(i = 0; i<array.length; i++){
   array.splice(i, 1, changeValue)
   if(array[i] === stopValue){
     break
