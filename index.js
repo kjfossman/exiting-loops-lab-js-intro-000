1e11038475cf6dc253ce84281f17ab0b8514ab98
Kyle Fossman <kjfossman@gmail.com>
2021-01-22 20:57:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ function breakOut(array, changeValue, stopValue){
 }
   return array
 }
+
+function keepGoing(array, changeValue, skipValue){
+  for(let i = 0; i<array.length; i++){
+    if(array[i] === skipValue){
+      continue
+    }
+    array.splice(i,1,changeValue)
+  }
+}
