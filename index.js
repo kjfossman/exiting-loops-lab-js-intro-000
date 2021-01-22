005906303e2252b070ca98c6974d2a5e3da44935
Kyle Fossman <kjfossman@gmail.com>
2021-01-22 20:52:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ function breakOut(array, changeValue, stopValue){
   for(let i = 0; i<array.length; i++){
   if(array[i] === stopValue){
     break
-  }array.splice(i, 1, changeValue)
+  }
+  array.splice(i, 1, changeValue)
 }
   return array
 }
