6bb638ea24138ddb0b73634a096dda29bdbb7568
Kyle Fossman <kjfossman@gmail.com>
2021-01-22 20:41:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function breakOut(array, changeValue, stopValue){
-  let changeValue = Math.floor(Math.random() * 100)
   for(let i = 0; i<array.length; i++){
   array.splice(i, 1, changeValue)
   if(array[i] === stopValue){
